cab00bfcd2be8fe128045182169a312a6bea52ed
22rodolphe02 <tsiafararodolphe@gmail.com>
2025-03-09 00:09:48 +0300

    [dev] creation de la page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,41 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+
+import Aura from '@primeng/themes/aura';
+import {definePreset} from '@primeng/themes';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-  provideHttpClient()]
+  provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: 'none'
+        }
+      }
+    })]
 };
+
---
+++ src/app/core/auth/auth.interceptor.ts
@@ -0,0 +1,36 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//   return next(req);
+// };
+
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const currentUser = this.authService.getCurrentUser();
+    if (currentUser && currentUser.token) {
+      // Ajouter le token à l'en-tête Authorization
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      });
+    }
+    return next.handle(request);
+  }
+}
