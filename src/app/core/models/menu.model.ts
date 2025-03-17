2cd0ef06d5bb495365a8a9416ce2dcb22cd89486
22rodolphe02 <tsiafararodolphe@gmail.com>
2025-03-17 17:11:29 +0300

    [fix] dynamiser les menu du header

---
+++ src/app/core/models/menu.model.ts
@@ -0,0 +1,6 @@
+export interface HeaderMenu{
+  id: number,
+  label: string,
+  icon?: string,
+  link: string,
+}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Avatar} from 'primeng/avatar';
+import {RouterLink, RouterModule} from '@angular/router';
+import {HeaderMenu} from '../../../core/models/menu.model';
 
 @Component({
   selector: 'g-header',
   imports: [
+    RouterModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  @Input({alias: 'menu'}) menus: HeaderMenu[] = []
 }
